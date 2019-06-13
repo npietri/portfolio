7055124b91583f06b0e8762516a583bcf85a4e41
npietri <nicolaspietri.fr@gmail.com>
2019-06-13 19:28:50 +0200

    création de 2 modules

---
+++ src/app/adminpage/adminpage.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { AdminpageRoutingModule } from './adminpage-routing.module';
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    AdminpageRoutingModule
+  ]
+})
+export class AdminpageModule { }
