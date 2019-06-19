28f186942228290af067e37dd99beb6d8f3b7f85
npietri <nicolaspietri.fr@gmail.com>
2019-06-19 08:49:52 +0200

    tous les components presentations + img presentation

--- src/app/mainpage/mainpage.module.ts
+++ src/app/mainpage/mainpage.module.ts
@@ -2,9 +2,14 @@ import { NgModule } from "@angular/core";
 import { CommonModule } from "@angular/common";
 
 import { MainpageRoutingModule } from "./mainpage-routing.module";
+import { EducationComponent } from './education/education.component';
+import { SkillsComponent } from './skills/skills.component';
+import { LanguesComponent } from './langues/langues.component';
+import { ContactComponent } from './contact/contact.component';
+import { PortfolioComponent } from './portfolio/portfolio.component';
 
 @NgModule({
-  declarations: [],
+  declarations: [EducationComponent, SkillsComponent, LanguesComponent, ContactComponent, PortfolioComponent],
   imports: [CommonModule, MainpageRoutingModule]
 })
 export class MainpageModule {}
