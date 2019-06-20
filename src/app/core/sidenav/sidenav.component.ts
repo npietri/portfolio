09e1b5be95d15fec5976df309e6f4a77c9ea75ad
npietri <nicolaspietri.fr@gmail.com>
2019-06-20 07:26:25 +0200

    sidebar

--- src/app/core/sidenav/sidenav.component.ts
+++ src/app/core/sidenav/sidenav.component.ts
@@ -1,28 +1,26 @@
-import { Component, OnInit, Output, EventEmitter } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { Component, OnInit, Output, EventEmitter } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
 
 @Component({
-  selector: 'app-sidenav',
-  templateUrl: './sidenav.component.html',
-  styleUrls: ['./sidenav.component.css']
+  selector: "app-sidenav",
+  templateUrl: "./sidenav.component.html",
+  styleUrls: ["./sidenav.component.css"]
 })
-
 export class SidenavComponent implements OnInit {
   @Output() sidenavClose = new EventEmitter();
 
   public entitiesLink: any[] = [
-    { entity: 'Admin', link: 'admin', icon: 'people' },
-    { entity: 'Expériences', link: 'experience', icon: 'texture' },
-    { entity: 'Projets', link: 'projets', icon: 'texture' },
-    { entity: 'Langues', link: 'langues', icon: 'library_books' },
-    { entity: 'Contact', link: 'contact', icon: 'library_books' },
+    { entity: "Admin", link: "login", icon: "people" },
+    { entity: "Expériences", link: "experience", icon: "texture" },
+    { entity: "Projets", link: "projets", icon: "texture" },
+    { entity: "Langues", link: "langues", icon: "library_books" },
+    { entity: "Contact", link: "contact", icon: "library_books" }
   ];
-  constructor(private httpClient: HttpClient) { }
+  constructor(private httpClient: HttpClient) {}
 
-  ngOnInit() {
-  }
+  ngOnInit() {}
 
   public onSidenavClose = () => {
     this.sidenavClose.emit();
-  }
+  };
 }
